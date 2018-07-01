52255b14dc935b50afbd2bd751eeeb1842e56585
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-07-01 18:58:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,7 @@ class TicTacToe
 
   def turn
     puts "1-9"
-    user_input = gets
+    user_input = gets.strip
     user_input = input_to_index(user_input)
     if valid_move?(user_input)
       move(user_input)
