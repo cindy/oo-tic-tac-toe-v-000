20963c20913e274334769ea0f365f24bcc0ac9fa
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-07-01 18:44:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,16 +1,27 @@
 class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
+    @WIN_COMBINATIONS = [
+      [0,1,2], # Top row
+      [3,4,5],  # Middle row
+      [6,7,8], # Bottom row
+      [0,4,8], # Right to left diagonal
+      [2,4,6],  # Left to right diagonal
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+    ]
+
   end
- 
+
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
- 
+
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
- 
+
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
